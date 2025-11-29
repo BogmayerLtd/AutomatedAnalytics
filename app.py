8c77cdf2c386be765d885a9e9089b26765c70215
BogmayerLtd <jackson@bogmayer.com>
2025-11-29 16:13:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def plot_inventory_bar(df):
     # Skip first 2 rows for all processing
     df = df.iloc[2:].copy()
     
-    if "Distributor Location" in df.columns and (df["Distributor Location"] == "Southern Crown Partners: Charleston, SC").any():
-        sub = df[df["Distributor Location"] == "Southern Crown Partners: Charleston, SC"].copy()
+    if "Location" in df.columns and (df["Location"] == "Southern Crown Partners: Charleston, SC").any():
+        sub = df[df["Location"] == "Southern Crown Partners: Charleston, SC"].copy()
         if "Product Name" in sub.columns and "On Floor Inventory (Cases)" in sub.columns:
             x = sub["Product Name"]
             y = sub["On Floor Inventory (Cases)"].fillna(0)
